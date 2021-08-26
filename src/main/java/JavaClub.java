9418b3cb7cfbf8542abc46e7045e9a279e30ee97
Wesley <81597212+Booked61@users.noreply.github.com>
2021-08-26 08:19:12 -0700

    Update JavaClub.java

--- src/main/java/JavaClub.java
+++ src/main/java/JavaClub.java
@@ -1,6 +1,5 @@
 import commands.Help;
 import commands.Info;
-import commands.Meme;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
